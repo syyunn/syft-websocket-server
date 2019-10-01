3314231edb2a5b21ff8e6fbf9e29ac94a7b5cf0c
Zachary <syyun@snu.ac.kr>
2019-10-01 11:30:06 +0900

    Try Pickling but failed

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ def activate_hook(torch):
     return sy.TorchHook(torch)
 
 
-def make_socket_serever(host, hook, id, port):
+def make_socket_serever(host, hook, id, port, log_msgs=True, verbose=True):
     from syft.workers.websocket_server import WebsocketServerWorker
-    server = WebsocketServerWorker(host=host, hook=hook, id=id, port=port)
+    server = WebsocketServerWorker(host=host, hook=hook, id=id, port=port,
+                                   log_msgs=log_msgs, verbose=verbose)
     return server
+
+
+# def pickle_socket_server(server, pickle_path="./server.pkl"):
+#     import pickle
+#     with open(pickle_path, 'wb') as f:
+#         pickle.dump(server, f)
 
 
 def main():
@@ -16,6 +23,7 @@ def main():
     id = 0
     port = 8182
     server = make_socket_serever(host=host, hook=hook, id=id, port=port)
+    # pickle_socket_server(server)
     server.start()
     # server.list_objects()
     # server.objects_count()
